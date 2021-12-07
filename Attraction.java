42f182d66e46ffe855d6c7dd162055b187967031
Eric Morgan <ericmorgan602@gmail.com>
2021-12-06 17:08:01 -0700

    added methods and instance variables

--- Attraction.java
+++ Attraction.java
@@ -1,5 +1,26 @@
 public class Attraction {
+    //constructor
     public Attraction() {
-        
+
     }
+    //method to calculate daily cost
+    public void dailyCost() {
+
+    }
+    //method to calculate profit
+    public void profit() {
+
+    }
+    //instance variables
+    private int maxRiders;
+    private int ticketsToUse;
+    private int attendantsAmount;
+    private int ridersPerHour;
+    private int hoursPerDay;
+    private double ticketCost;
+    private double height;
+    private double utilitiesCost;
+    private double rideLength;
+    private double speed;
+
 }
