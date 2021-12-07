bec955d0d3c2bb2ae235d9d2918e2d137761c5b9
alexzlmao <s24400008@stu.tempeunion.org>
2021-12-07 13:58:03 -0700

    Update Attraction.java

--- Attraction.java
+++ Attraction.java
@@ -4,21 +4,28 @@ public class Attraction {
 	
 	//constructor
     
-	public Attraction(int a, int b, double c, double d, double e, double f) {
+	public Attraction(int a, int b, double c, double d, double e, double f, int g, double h) {
     		maxRiders = a;
     		hoursPerDay = b;
     		ticketCost = c;
     		height = d;
     		speed = e;
     		rideLength = f;
+    		riderAmount = g;
+    		rideCare = h;
     }
     //method to calculate daily cost
-    public void dailyCost() {
-
+    public double dailyCost(int k, double l) { //k is hoursPerDay, l is rideCare
+    		return (double)(k*l);
     }
+  
+    
     //method to calculate profit
-    public void profit() {
-
+    public double profit(int k, double l, double i, int j) {  //i is ticketCost, j is riderAmount
+		
+    	double cost2 = (double)dailyCost(k, l);
+		return (double)((i*j) - cost2);
+    		
     }
     //instance variables
     private int maxRiders; //the maximum number of riders a ride can have
@@ -27,5 +34,6 @@ public class Attraction {
     private double height; //the height of a ride
     private double rideLength; //the length of a ride
     private double speed; //the speed of a ride
-
+    private int riderAmount;
+    private double rideCare;
 }
