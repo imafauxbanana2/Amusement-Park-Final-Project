bd545e0c6dc1ffbda0d893108d253be32cde46a8
alexzlmao <s24400008@stu.tempeunion.org>
2021-12-08 12:56:40 -0700

    [[e

--- AttractionTest.java
+++ AttractionTest.java
@@ -1,7 +1,7 @@
 public class AttractionTest {
 	public static void main (String[]args) {
-		Attraction connect = new Attraction();
-		
+		Attraction connect = new Attraction(20, 8, 5.0, 42, 40, 10, 20, 150);
+			System.out.println(connect.profit());
 		
 		
 	}
