e51004c3ca138b7848aeef3949541c5287aa4aad
Eric Morgan <ericmorgan602@gmail.com>
2021-12-08 12:59:02 -0700

    added test class

--- AttractionTest.java
+++ AttractionTest.java
@@ -1,7 +1,7 @@
 public class AttractionTest {
 	public static void main (String[]args) {
 		Attraction connect = new Attraction(20, 8, 5.0, 42, 40, 10, 20, 150);
-			System.out.println(connect.profit());
+			System.out.println(connect.profit(8, 120, 5, 20));
 		
 		
 	}
