832afba4f1a60b07161e5c1dbc1c959f5acc0180
Eric Morgan <ericmorgan602@gmail.com>
2021-12-09 14:04:11 -0700

    boots n cats

--- src/HauntedMansion.java
+++ src/HauntedMansion.java
@@ -1,3 +1,10 @@
-public class HauntedMansion {
+public class HauntedMansion extends Attraction {
+
+    public HauntedMansion(int a, int b, double c, double d, double e, double f, int g, double h, int p) {
+        super(a, b, c, d, e, f, g, h, p);
+    }
+
+    private int ridersScared;
+
     
 }
