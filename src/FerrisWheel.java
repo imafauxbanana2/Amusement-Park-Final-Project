4bbaaf32d78de254004b1a3c001c393625a3dc62
MatthewJimenez04 <88420554+MatthewJimenez04@users.noreply.github.com>
2021-12-13 08:56:56 -0700

    Update FerrisWheel.java
    About Finished Up

--- src/FerrisWheel.java
+++ src/FerrisWheel.java
@@ -5,3 +5,34 @@ public class FerrisWheel extends Attraction {
 		
 		
 }
+public class FerrisWheel extends Attraction {
+	
+	  public FerrisWheel(int a, int b, double c, double d, double e, double f, int g, double h, int p) 
+	  {
+	        super(a, b, c, d, e, f, g, h, p);
+	  }
+	
+	  public static void gForce()
+	  {		  
+		  	//Calculate gForce by dividing its average acceleration by the acceleration due to gravity which is 32 ft/s^2
+	        double gForce= (1.5/32);
+	        System.out.println("The g-force of the ride is: " + String.format("%1.2f", gForce) + " G's! \n");
+	  }
+	  
+	  public static void heightFright() 
+	  {
+	        int heightFright = (int)(Math.random() * 10 + 1);
+	        System.out.println("This Month, while on the ride " + heightFright + " people puked because of their fears of height!");
+	  }  
+	  
+	  public static void main(String args[]) 
+	  {
+		  Attraction BigO = new Attraction(60, 8, 7.50, 36.0, 1.5, 900, 20, 150.0, 20);
+		
+		  System.out.println ("Welcome to the BigO:");
+		  FerrisWheel.gForce();
+		  FerrisWheel.heightFright();
+		  System.out.println("\nThe daily cost to operate the ride is: $" + BigO.dailyCost(8, 150.0));
+		  System.out.println("\nThe profit of the ride is: $" + BigO.profit());
+	  }		
+}
