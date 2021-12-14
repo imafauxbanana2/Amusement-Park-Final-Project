68a9e51bf3c322295a3a935ef05cbd52d2bfdd8b
MatthewJimenez04 <88420554+MatthewJimenez04@users.noreply.github.com>
2021-12-14 13:54:27 -0700

    Update HauntedMansion.java

--- src/HauntedMansion.java
+++ src/HauntedMansion.java
@@ -1,6 +1,18 @@
 //@author Eric Morgan
 public class HauntedMansion extends Attraction {
 
+        /**
+		 * @param a- the maximum number of riders a ride can have
+		 * @param b- how many hours per day the ride operates
+		 * @param c- the tickets a ride costs
+		 * @param d- the height to get into a ride in inches
+		 * @param e- the speed of a ride
+		 * @param f- the length of a ride in seconds
+		 * @param g- number of riders on a ride
+		 * @param h- cost for ride upkeep
+		 * @param p- the number of times a ride is ridden
+		 */
+    
     public HauntedMansion(int a, int b, double c, double d, double e, double f, int g, double h, int p) {
         super(a, b, c, d, e, f, g, h, p);
     }
